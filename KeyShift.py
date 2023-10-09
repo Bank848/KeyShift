f1deb50064981591791c88c18077044ef431a4db
Bank <65855292+Bank848@users.noreply.github.com>
2023-10-09 12:43:56 +0700

    Update KeyShift.py

--- KeyShift.py
+++ KeyShift.py
@@ -1,32 +1,7 @@
 import os
-import shutil
 import re
 import sys
 import subprocess
-
-# Python version 3.11.5
-# Code from ChatGPT
-# Made By Bank's : Thai translator H Game
-# Link Discord : https://discord.gg/q6FkGCHv66
-
-# โปรแกรมนี้ใช้เพื่อคัดแยกไฟล์ที่ตรงกับคำค้นหาแล้วย้ายไฟล์นั้นออกมาไว้ในโฟลเดอร์ใหม่
-# This program is used to extract files that match a search term and move them into a new folder.
-
-# How to use on PC
-# Just input Folder paths, and the file extension
-
-# วิธีใช้บนคอม
-# เพียงแค่ป้อนตำแหน่งของโฟลเดอร์ และนามสกุลของไฟล์นั้น ๆ 
-
-# How to use in moblie version
-# Please run in Pydroid 3 - IDE for Python 3
-# Then Just input Folder paths and the file extension
-# If you put flie in Pyroid3 Folder the path is [/storage/emulated/0/Documents/Pydroid3/(Put your folder name here)"]
-
-# วิธีใช้บนมือถือ
-# ให้รันบนPydroid 3 - IDE for Python 3
-# ถ้าเอาไฟล์ดิบที่ต้องการไว้ในโฟลเดอร์  Pyroid3 ตำแหน่งไฟล์คือ [/storage/emulated/0/Documents/Pydroid3/(ใส่ชื่อโฟลเดอร์ตรงนี้)]
-# เพียงแค่ป้อนตำแหน่งของโฟลเดอร์ และนามสกุลของไฟล์นั้น ๆ 
 
 # Check if tqdm is installed, and if not, install it
 try:
@@ -36,102 +11,59 @@ except ImportError:
     subprocess.run([sys.executable, "-m", "pip", "install", "tqdm"])
     from tqdm import tqdm
 
-def get_valid_folder_path(folder_paths):
-    while True:
-        folder_path = input("Please enter the folder path or type 'done' to finish: ")
-        if folder_path.lower() == 'done':
-            if not folder_paths:
-                print("No folder paths have been added yet!")
-                continue
-            return None
-        if os.path.isdir(folder_path):
-            return folder_path
-        else:
-            print("Invalid folder path. Please try again.")
+def to_raw_string(s):
+    return s.encode('unicode_escape').decode()
 
-def sanitize_folder_name(keyword):
-    # List of invalid characters for folder names on Windows
-    invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
-    for char in invalid_chars:
-        keyword = keyword.replace(char, '')
-    # If after removing invalid chars the keyword becomes empty, use a default name
-    if not keyword:
-        return "WithKeyword"
-    return f"With Keyword ({keyword})"
+def has_english(text):
+    return bool(re.search('[A-Za-z]', text))
 
-def main_program():
-    print("""
-Python version 3.11.5
-Code from ChatGPT
-Made By Bank's : Thai translator H Game
-Link Discord : https://discord.gg/q6FkGCHv66
-โปรแกรมนี้ใช้เพื่อแยกไฟล์ที่ตรงกับคำค้นหาแล้วแยกไฟล์ออกมาไว้ในโฟลเดอร์
-This program is used to extract files that match a search term and place them in a folder.
-How to use on PC
-Just input Folder paths, and the file extension
-วิธีใช้บนคอม
-เพียงแค่ป้อนตำแหน่งของโฟลเดอร์ และนามสกุลของไฟล์นั้น ๆ 
-How to use in moblie version
-Please run in Pydroid 3 - IDE for Python 3
-Then Just input Folder paths and the file extension
-If you put flie in Pyroid3 Folder the path is [/storage/emulated/0/Documents/Pydroid3/(Put your folder name here)"]
-วิธีใช้บนมือถือ
-ให้รันบนPydroid 3 - IDE for Python 3
-ถ้าเอาไฟล์ดิบที่ต้องการไว้ในโฟลเดอร์  Pyroid3 ตำแหน่งไฟล์คือ [/storage/emulated/0/Documents/Pydroid3/(ใส่ชื่อโฟลเดอร์ตรงนี้)]
-เพียงแค่ป้อนตำแหน่งของโฟลเดอร์ และนามสกุลของไฟล์นั้น ๆ 
-"""
-    )
+def read_all_txt_files_in_directory(directory):
+    all_contents = {}
+    files = [f for f in os.listdir(directory) if f.lower().endswith('.txt')]
+    for filename in tqdm(files, desc="Reading files"):
+        filepath = os.path.join(directory, filename)
+        try:
+            with open(filepath, 'r', encoding='utf8') as f:
+                content = f.read()
+            all_contents[filepath] = content
+        except Exception as e:
+            print(f"Error occurred while reading {filepath}: {e}")
+
+    return all_contents
+
+def extract_english_text_from_content(content):
+    english_texts = []
+    lines = content.splitlines()
+    for i in tqdm(range(len(lines) - 1), desc="Extracting English texts"):
+        if lines[i].strip() == '[0]' and '1 string data =' in lines[i+1]:
+            match = re.search(r'1 string data = "(.*?)"', lines[i+1])
+            if match:
+                matched_text = match.group(1)
+                if has_english(matched_text):
+                    english_texts.append(matched_text)
+    return english_texts
+
+def save_english_texts_to_txt(texts, filename):
+    # Ensure the directory exists
+    output_dir = "C:\\OUTPUT FOLDER"  # แก้ไขเส้นทางของไฟล์ให้ถูกต้อง
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    output_filename = os.path.join(output_dir, "extracted_texts.txt")
+    with open(output_filename, 'a', encoding='utf8') as file:
+        for text in texts:
+            file.write(text + '\n')
+    print(f"English texts saved to {output_filename}.")
+
+directory = input("Enter the path to your directory containing txt files: ")
+directory = to_raw_string(directory)
+all_file_contents = read_all_txt_files_in_directory(directory)
+
+for filepath, content in all_file_contents.items():
+    english_texts = extract_english_text_from_content(content)
     
-    # Get folder paths
-    folder_paths = set()
-
-    while True:
-        folder_path = get_valid_folder_path(folder_paths) # Pass folder_paths as an argument
-        if folder_path is None:
-            break
-        if folder_path in folder_paths:
-            print("This folder path has already been added.")
-        else:
-            folder_paths.add(folder_path)
-
-    folder_paths = list(folder_paths)
-
-    # Get file extension
-    file_extension = input("Please enter the file extension you want to search for (e.g. .txt, .docx): ")
-
-    # Get keyword
-    keyword = input("Enter the keyword to search for: ")
-
-    # Set up the new folder for files containing the keyword
-    new_folder_name = sanitize_folder_name(keyword)
-    new_folder_path = os.path.join(os.path.dirname(folder_paths[0]), new_folder_name)
-
-    # Create the new folder if it doesn't exist
-    if not os.path.exists(new_folder_path):
-        os.makedirs(new_folder_path)
-
-    files_to_search = []
-
-    # Loop through each folder to gather files to search
-    for folder_path in folder_paths:
-        for filename in os.listdir(folder_path):
-            if filename.endswith(file_extension):
-                file_path = os.path.join(folder_path, filename)
-                files_to_search.append(file_path)
-
-    # Check for the pattern 'string data = "..."' in the content
-    for file_path in tqdm(files_to_search, desc="Searching files", unit="file"):
-        with open(file_path, 'r', encoding='utf8') as f:
-            content = f.read()
-        if re.search(r'string data = ".*?"', content):
-            new_file_path = os.path.join(new_folder_path, os.path.basename(file_path))
-            shutil.move(file_path, new_file_path)
-
-# Main loop to repeat the process
-while True:
-    main_program()
-
-    # Ask user if they want to repeat the process
-    repeat = input("Do you want to repeat the process? (yes/no): ").lower()
-    if repeat != 'yes':
+    choice = input(f"Would you like to save the English text from the txt file {filepath} to a txt file? (yes/no) ").strip().lower()
+    if choice == 'yes':
+        save_english_texts_to_txt(english_texts, filepath)
+    elif choice == 'no':
         break
